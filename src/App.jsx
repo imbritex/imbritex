de0571db5a35798372b5c154b0abd0ac46368d33
expertyeti <leivetd@gmail.com>
2026-09-01 02:08:10 -0600

    Agregar comentario

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Projects from './components/Projects'
 
+// Componente principal de la aplicación
 function App() {
   return (
     <div className="max-w-4xl mx-auto p-8 min-h-screen flex flex-col">
